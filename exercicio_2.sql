33410b1012f63a2f7c9c93efb0484083e263166e
André Henrique <andrehenriquenascimento1@gmail.com>
2023-09-25 09:56:16 -0300

    exercicio 2

---
+++ exercicio_2.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('Romance');
